3c1f2dbec81e1b3cd39a605403af6c90e1996e71
Vasily <vasa-svistun@mail.ru>
2021-01-13 13:23:35 +0300

    Свёрстан блок интро и шапка,добавлен адаптив,оптимизированы фотографии

--- #source/js/main.js
+++ #source/js/main.js
@@ -1,4 +1,43 @@
 document.addEventListener("DOMContentLoaded", function () {
-  //@prepros-append my-scripts.js
   //@prepros-append dynamicAdapt.js
+
+  // Turn img to background ===========================================
+
+  const ibgBlocks = document.querySelectorAll(".ibg");
+
+  function setImgToBackground() {
+    ibgBlocks.forEach((block) => {
+      let image = block.querySelector("img");
+
+      block.style.cssText = `
+			background: url(${image.getAttribute("src")}) center no-repeat;
+			background-size : cover;
+		`;
+
+      image.style.display = "none";
+    });
+  }
+
+  setImgToBackground();
+
+  // Intro menu ===========================================
+
+  const burgerMenuBtn = document.querySelector(".header__burger"),
+    introWrapper = document.querySelector(".intro__wrapper"),
+    introMenu = document.querySelector(".intro__menu"),
+    introBackground = document.querySelector(".intro__background"),
+    header = document.querySelector(".header");
+
+ 
+  burgerMenuBtn.addEventListener("click", showIntroMenu);
+  
+
+  function showIntroMenu(params) {
+    this.classList.toggle("active");
+    introWrapper.classList.toggle("active");
+    introMenu.classList.toggle("active");
+    introBackground.classList.toggle("active");
+    header.classList.toggle("active");
+    document.body.classList.toggle("lock");
+  }
 });
